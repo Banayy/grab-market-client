7286ed1250dcb2f557dc4a3a41aa945e23104f05
박성준 <parksungjun@bagseongjun-ui-MacBookAir.local>
2022-04-17 15:27:28 +0900

    api URL 주소변경

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,3 +1,6 @@
 /* react project의 변수들을 관리 */
 
-export const API_URL = "http://localhost:8080";
+export const API_URL =
+  process.env.NODE_ENV === "production"
+    ? "https://grab-market-server-p.herokuapp.com"
+    : "http://localhost:8080";
